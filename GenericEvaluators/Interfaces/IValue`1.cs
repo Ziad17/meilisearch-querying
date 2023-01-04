166800a5559730d65c32ddeaee25593fc70b8d17
ziadmohamd456 <ziadmohamd456@gmail.com>
2023-01-04 13:15:07 +0200

    reconfigure evaluators interfaces

---
+++ GenericEvaluators/Interfaces/IValue`1.cs
@@ -0,0 +1,25 @@
+﻿using FluentSearchEngine.Paging;
+using Meilisearch;
+using System.Linq.Expressions;
+
+namespace FluentSearchEngine.GenericEvaluators.Interfaces
+{
+    public interface IValue<T> : IFilter
+    {
+        IStringsEvaluator<T> Value(Expression<Func<T, string>> action);
+
+        ICollectionEvaluator<T> Value<TData>(Expression<Func<T, ICollection<TData>>> action);
+
+        IBooleanEvaluator<T> Value(Expression<Func<T, bool>> action);
+
+        INumbersEvaluator<T> Value(Expression<Func<T, int>> action);
+
+        INumbersEvaluator<T> Value(Expression<Func<T, double>> action);
+
+        INumbersEvaluator<T> Value(Expression<Func<T, decimal>> action);
+
+        SearchQuery Evaluate(PageCriteria pageCriteria);
+
+        SearchQuery Evaluate(int pageNumber = 1, int pageSize = 10);
+    }
+}
